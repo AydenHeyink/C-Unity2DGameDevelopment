703405f19af38a803118ef14284e814b771a0643
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-08-20 22:22:14 -0400

    August 20 part 2

--- DeliveryDash/Assets/Scripts/Collision.cs
+++ DeliveryDash/Assets/Scripts/Collision.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    [SerializeField] float delay = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up package");
+            hasPackage = true;
+            Destroy(collision.gameObject, delay);
         }
-        if (collision.CompareTag("Customer"))
+        if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered package");
+            hasPackage = false;
         }
 
         //Debug.Log("What the heck was that?");
