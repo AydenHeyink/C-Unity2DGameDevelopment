4cf468f5663ce71b6ca3283b3131f36fe5e6fa72
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-09-12 13:26:38 -0400

    September 12 part 1

--- DeliveryDriver/Assets/Scripts/Drver.cs
+++ DeliveryDriver/Assets/Scripts/Drver.cs
@@ -14,7 +14,10 @@ public class Drver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);    
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);    
     }
 }
