539994a1c025e8393c7f01a93a60b9c8910a2cf2
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-10-12 00:44:42 -0400

    October 11 part 1

--- Tilemania/Tilemania/Assets/Scripts/PlayerMovement.cs
+++ Tilemania/Tilemania/Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,26 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput playerInput;
     private InputAction moveAction;
     private Rigidbody2D rb;
+    Animator myAnimator;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private float jumpSpeed = 20f;
 
     private Vector2 moveInput;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        myAnimator= GetComponent<Animator>();
 
         moveAction = new InputAction(type: InputActionType.Value, binding: "<Keyboard>/w");
         moveAction.AddCompositeBinding("2DVector")
             .With("Up", "<Keyboard>/w")
             .With("Down", "<Keyboard>/s")
             .With("Left", "<Keyboard>/a")
-            .With("Right", "<Keyboard>/d");
+            .With("Right", "<Keyboard>/d")
+            .With("Jump", "<Keyboard>/space")
+            ;
 
         moveAction.Enable();
     }
@@ -40,5 +45,27 @@ public class PlayerMovement : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x, 0);
         moveInput = moveAction.ReadValue<Vector2>();
         rb.velocity = moveInput * moveSpeed;
+        bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        myAnimator.SetBool("isRunning", hasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+
+        }
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
 }
