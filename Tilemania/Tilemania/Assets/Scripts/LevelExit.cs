210385bbf5abb2118b7e5afb38fea84ee623c29a
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-10-24 15:13:35 -0400

    October 24 part 1

--- Tilemania/Tilemania/Assets/Scripts/LevelExit.cs
+++ Tilemania/Tilemania/Assets/Scripts/LevelExit.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 3;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
