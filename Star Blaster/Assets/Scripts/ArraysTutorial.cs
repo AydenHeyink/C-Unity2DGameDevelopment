6cd43783eda288735d67d5a627202acdc856236d
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-10-30 17:09:58 -0400

    October 30 part 1

---
+++ Star Blaster/Assets/Scripts/ArraysTutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysTutorial : MonoBehaviour
+{
+    int[] numbers = { 5, 10, 15, 20 };
+
+    void Start()
+    {
+
+    }
+}
