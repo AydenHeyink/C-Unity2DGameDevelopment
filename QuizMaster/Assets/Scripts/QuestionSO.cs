da2931ea762ce5bdf682980a01b3471aa875b819
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-11-21 15:00:52 -0500

    November 21 part 1

---
+++ QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea()]
+    [SerializeField] string question = "Enter new question text here";
+}
