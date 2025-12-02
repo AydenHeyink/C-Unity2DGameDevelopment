24b838debc6c66b9a25439277fd4940225bf739a
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-12-01 21:05:43 -0500

    December 1 part 1

--- QuizMaster/Assets/Scripts/Quiz.cs
+++ QuizMaster/Assets/Scripts/Quiz.cs
@@ -25,6 +25,10 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
     void Start()
     { 
         timer = FindObjectOfType<Timer>();
@@ -55,6 +59,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score : " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -66,6 +71,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else 
         {
@@ -86,6 +92,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
     }
 
