e20ec01bc15a12e188bc9ef4a40c6b78807b6114
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-12-16 14:23:41 -0500

    December 16 part 1

--- TileVania/TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/TileVania/Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    float moveSpeed = 5f;
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log("");
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity ;
     }
 }
