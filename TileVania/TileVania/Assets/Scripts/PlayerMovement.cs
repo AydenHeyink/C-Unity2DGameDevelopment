c0bf867b0b121f6d4d41fa344a67907503c33156
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2025-12-17 17:17:01 -0500

    December 17 part 1

--- TileVania/TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/TileVania/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     float moveSpeed = 5f;
+    Animator myAnimator;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator= GetComponent<Animator>();
     }
 
     void Update()
@@ -27,5 +29,18 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity ;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", true);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
